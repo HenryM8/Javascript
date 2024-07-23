d97c9ab2944285b4644821157bcef433cb6c6469
HenryM8 <henryrm8@gmail.com>
2024-07-23 09:51:20 -0600

    Mínimo

---
+++ Practicas.js/EjerciciosPag55/Minimo.js
@@ -0,0 +1,12 @@
+/*
+Mínimo
+
+El capítulo previo presentó la función estándar Math.min que devuelve su menor
+argumento. Ahora podemos escribir una función como esa nosotros mismos.
+Define la función min que toma dos argumentos y devuelve su mínimo.
+*/
+
+function min(a, b) {
+  return 'Argumento menor: ' + Math.min(a, b)
+}
+console.log(min(500, 501))
